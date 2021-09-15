90e6ab31bbe29f10f91a86af4d75cd6ab69d908a
nayan-kumar-ghub <messages46@gmail.com>
2021-09-15 08:58:14 -0700

    initial commit

---
+++ Qualys_Agent_Install.ps1
@@ -0,0 +1,4 @@
+﻿$TempPath = 'C:\temp\'
+if(!(Test-Path $TempPath)) {New-Item -Type Directory -Path $TempPath -Verbose}
+Start-BitsTransfer -source http://fs2.catchpoint.com/UtilFiles/QualysCloudAgent.exe -Destination $TempPath\QualysCloudAgent.exe
+"c:\temp\QualysCloudAgent.exe CustomerId={36623f99-acc4-5a56-83e4-3c497b49d4a8} ActivationId={0e56dc46-d39b-4b1c-92eb-1f3d9a8be97e} WebServiceUri=https://qagpublic.qg3.apps.qualys.com/CloudAgent/" | cmd
